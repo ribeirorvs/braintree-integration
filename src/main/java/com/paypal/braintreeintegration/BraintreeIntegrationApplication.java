635f9b9df05321ff1d1bf7ad7d698c7a9046c4ff
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2022-03-17 18:16:53 -0300

    Create project structure

---
+++ src/main/java/com/paypal/braintreeintegration/BraintreeIntegrationApplication.java
@@ -0,0 +1,22 @@
+package com.paypal.braintreeintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class BraintreeIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BraintreeIntegrationApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name){
+		return String.format("Hello %s!", name);
+	}
+
+}
---
+++ src/test/java/com/paypal/braintreeintegration/BraintreeIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paypal.braintreeintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BraintreeIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
