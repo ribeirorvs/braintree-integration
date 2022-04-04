9e0ce9e40116182ec4bc630ff27f86e6507fa79d
Rodrigo Vitor Ribeiro <ribeiro.rvs@hotmail.com>
2022-04-04 16:19:17 -0300

    Configure the PayPal Plus render

--- src/main/resources/static/js/ppplus.js
+++ src/main/resources/static/js/ppplus.js
@@ -1,14 +1,33 @@
+var ppp;
+
 function takeAuthToken(){
     $.ajax({
         type: 'GET',
         url: '/aToken'
-        }).done(function(result) {
-            alert(result)
+    }).done(function(result) {
+            $.ajax({
+                type: 'GET',
+                url: '/create-payment',
+                data: {token: result}
+            }).done(function(result) {
+                ppp = PAYPAL.apps.PPP({
+                    "approvalUrl": result,
+                    "placeholder": "ppplus",
+                    "mode": "sandbox",
+                    "payerFirstName": "Eu",
+                    "payerLastName": "Mesmo",
+                    "payerPhone": "11900000000",
+                    "payerEmail": "email@email.com",
+                    "payerTaxId": "37128723803",
+                    "payerTaxIdType": "BR_CPF",
+                    "language": "pt_BR",
+                    "country": "BR",
+                    "rememberedCards": "customerRememberedCardHash",
+                    "enableContinue": "continueButton",
+                });
+            })
     });
 }
 /*
-var ppp = PAYPAL.apps.PPP({
-    "approvalUrl": "'.$approval_url.'",
-    "placeholder": "ppplus",
-    "mode": "sandbox",});
+
 */
